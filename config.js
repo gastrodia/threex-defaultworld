2034571c7b2c63902848dabd3d7f7b180193c6df
ELatA <elata@foxmail.com>
2014-04-14 13:24:43 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+require.config({
+  shim: {
+    three: {
+      exports: "THREE"
+    },
+    "threex-controls": {
+       deps: [
+        "three"
+       ]
+    }
+  },
+  paths: {
+    three: "bower_components/three.js/three.min",
+    requirejs: "bower_components/requirejs/require",
+    "threex-controls": "bower_components/threex-controls/controls/OrbitControls"
+  }
+});
